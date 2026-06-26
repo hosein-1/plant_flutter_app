7a3c2d150b3b2df5bae853db31b6fb3bb759f0ef
hosein-1 <hoseinlaki16@gmail.com>
2026-06-26 15:59:04 +0330

    Implement products list page

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
 import 'package:plant_app/const/constants.dart';
+import 'package:plant_app/models/plant.dart';
+import 'package:plant_app/screens/detail_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +13,11 @@ class HomePage extends StatefulWidget {
 
 class _ScanPageState extends State<HomePage> {
   int selectedIndex = 0;
+  final List<Plant> _plantList = Plant.plantList;
+  bool toggleFavorite(bool isFavorite) {
+    return !isFavorite;
+  }
+
   final List<String> _plantTypes = [
     '| پیشنهادی |',
     '| آپارتمانی |',
@@ -110,9 +118,265 @@ class _ScanPageState extends State<HomePage> {
                 },
               ),
             ),
+            //product one
+            SizedBox(
+              height: size.height * 0.3,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                reverse: true,
+                itemCount: _plantList.length,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        PageTransition(
+                          child: const DetailPage(),
+                          type: PageTransitionType.topToBottom,
+                        ),
+                      );
+                    },
+                    child: Container(
+                      width: 200.0,
+                      margin: const EdgeInsets.symmetric(horizontal: 18.0),
+                      decoration: BoxDecoration(
+                        color: Constants.primaryColor.withValues(alpha: 0.8),
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      child: Stack(
+                        children: <Widget>[
+                          Positioned(
+                            top: 10.0,
+                            right: 20.0,
+                            child: Container(
+                              height: 40.0,
+                              width: 40.0,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(50.0),
+                              ),
+                              child: IconButton(
+                                onPressed: () {
+                                  setState(() {
+                                    bool isFavorited = toggleFavorite(
+                                      (_plantList[index].isFavorated),
+                                    );
+                                    _plantList[index].isFavorated = isFavorited;
+                                  });
+                                },
+                                icon: Icon(
+                                  _plantList[index].isFavorated == true
+                                      ? Icons.favorite
+                                      : Icons.favorite_border_outlined,
+                                  color: Constants.primaryColor,
+                                  size: 20.0,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            left: 50.0,
+                            right: 50.0,
+                            top: 50.0,
+                            bottom: 50.0,
+                            child: Image.asset(_plantList[index].imageURL),
+                          ),
+                          Positioned(
+                            bottom: 15.0,
+                            left: 20.0,
+                            child: Container(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 10.0,
+                                vertical: 2.0,
+                              ),
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(20.0),
+                              ),
+                              child: Text(
+                                r'$' +
+                                    _plantList[index].price
+                                        .toString()
+                                        .farsiNumber,
+                                style: TextStyle(
+                                  color: Constants.primaryColor,
+                                  fontSize: 16.0,
+                                  fontFamily: 'Lalezar',
+                                ),
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            bottom: 15.0,
+                            right: 20.0,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: <Widget>[
+                                Text(
+                                  _plantList[index].category,
+                                  style: const TextStyle(
+                                    fontFamily: 'BYekan',
+                                    color: Colors.white70,
+                                    fontSize: 14.0,
+                                  ),
+                                ),
+                                Text(
+                                  _plantList[index].plantName,
+                                  style: const TextStyle(
+                                    fontFamily: 'BYekan',
+                                    color: Colors.white70,
+                                    fontSize: 14.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            // new plants - text
+            Container(
+              padding: const EdgeInsets.only(
+                right: 18.0,
+                bottom: 15.0,
+                top: 20.0,
+              ),
+              alignment: Alignment.centerRight,
+              child: const Text(
+                'گیاهان جدید',
+                style: TextStyle(
+                  fontFamily: 'Lalezar',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18.0,
+                ),
+              ),
+            ),
+            // product two
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 18.0),
+              height: size.width * 0.52,
+              child: ListView.builder(
+                itemCount: _plantList.length,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        PageTransition(
+                          child: const DetailPage(),
+                          type: PageTransitionType.topToBottom,
+                        ),
+                      );
+                    },
+                    child: Container(
+                      height: 80.0,
+                      width: size.width,
+                      margin: const EdgeInsets.only(bottom: 10.0, top: 10.0),
+                      padding: const EdgeInsets.only(left: 10.0, top: 10.0),
+                      decoration: BoxDecoration(
+                        color: Constants.primaryColor.withValues(alpha: 0.1),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Row(
+                            children: [
+                              SizedBox(
+                                height: 20.0,
+                                child: Image.asset(
+                                  'assets/images/PriceUnit-green.png',
+                                ),
+                              ),
+                              const SizedBox(width: 5.0),
+                              Text(
+                                _plantList[index].price.toString().farsiNumber,
+                                style: TextStyle(
+                                  fontFamily: 'Lalezar',
+                                  color: Constants.primaryColor,
+                                  fontSize: 20.0,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Stack(
+                            clipBehavior: Clip.none,
+                            children: <Widget>[
+                              Container(
+                                width: 60.0,
+                                height: 60.0,
+                                decoration: BoxDecoration(
+                                  color: Constants.primaryColor.withValues(
+                                    alpha: 0.8,
+                                  ),
+                                  shape: BoxShape.circle,
+                                ),
+                              ),
+                              Positioned(
+                                bottom: 5.0,
+                                left: 0.0,
+                                right: 0.0,
+                                child: SizedBox(
+                                  height: 80.0,
+                                  child: Image.asset(
+                                    _plantList[index].imageURL,
+                                  ),
+                                ),
+                              ),
+                              Positioned(
+                                bottom: 5.0,
+                                right: 80.0,
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.end,
+                                  children: <Widget>[
+                                    Text(
+                                      _plantList[index].category,
+                                      style: const TextStyle(
+                                        fontSize: 13.0,
+                                        fontFamily: 'BYekan',
+                                      ),
+                                    ),
+                                    Text(
+                                      _plantList[index].plantName,
+                                      style: TextStyle(
+                                        fontSize: 18.0,
+                                        fontFamily: 'BYekan',
+                                        color: Constants.blackColor,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
           ],
         ),
       ),
     );
+  }
+}
+
+extension FarsiNumberExtension on String {
+  String get farsiNumber {
+    const english = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    const farsi = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
+    String text = this;
+    for (int i = 0; i < english.length; i++) {
+      text = text.replaceAll(english[i], farsi[i]);
+    }
+    return text;
   }
 }
