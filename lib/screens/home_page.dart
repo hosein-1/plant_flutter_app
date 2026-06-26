b9276c264b578e46d402a101aef80617ea62d5c9
hosein-1 <hoseinlaki16@gmail.com>
2026-06-26 12:09:33 +0330

    Add search bar

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:plant_app/const/constants.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _ScanPageState();
+}
+
+class _ScanPageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: Center(
+                child: Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  width: size.width * 0.9,
+                  decoration: BoxDecoration(
+                    color: Constants.primaryColor.withValues(alpha: 0.1),
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        Icons.mic,
+                        color: Constants.blackColor.withValues(alpha: 0.6),
+                      ),
+                      const Expanded(
+                        child: Directionality(
+                          textDirection: TextDirection.rtl,
+                          child: TextField(
+                            showCursor: false,
+                            decoration: InputDecoration(
+                              contentPadding: EdgeInsets.only(right: 5.0),
+                              hintText: 'جستجو...',
+                              border: InputBorder.none,
+                              focusedBorder: InputBorder.none,
+                            ),
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                              fontFamily: 'iranSans',
+                              fontSize: 14.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Icon(
+                        Icons.search,
+                        color: Constants.blackColor.withValues(alpha: 0.6),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
